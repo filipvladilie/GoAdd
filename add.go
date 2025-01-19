65a7c7c0715b86c78d359e5279f4204fb65c4c56
Vlad-Ilie Filip <vlad-iliefilip@Vlad-Ilies-MacBook-Pro.local>
2025-01-19 22:12:02 +0200

    Add godoc format

--- add.go
+++ add.go
@@ -1,5 +1,13 @@
 package goadd
 
+// Add takes two integers, a and b, and returns their sum.
+// 
+// Parameters:
+//   - a: The first integer.
+//   - b: The second integer.
+//
+// Returns:
+//   The sum of a and b.
 func Add(a, b int) int {
   return a + b
 }
