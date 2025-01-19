3a7adae7b94765e5f3bbf7b3ac97642e59c4772f
Vlad-Ilie Filip <vlad-iliefilip@Vlad-Ilies-MacBook-Pro.local>
2025-01-19 22:26:25 +0200

    Add generic format

--- add.go
+++ add.go
@@ -1,5 +1,11 @@
 package goadd
 
+import goconstraint "golang.org/x/exp/constraints"
+
+type Number interface {
+  goconstraint.Integer | goconstraint.Float
+}
+
 // Add takes two integers, a and b, and returns their sum.
 // 
 // Parameters:
@@ -8,6 +14,6 @@ package goadd
 //
 // Returns:
 //   The sum of a and b.
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
   return a + b
 }
